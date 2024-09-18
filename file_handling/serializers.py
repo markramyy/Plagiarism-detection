ca48677abb87b56efd3700d59b8f1782ddb7c535
Mark Ramy <markramyy@gmail.com>
2024-09-18 22:33:16 +0300

    fix possible error

--- file_handling/serializers.py
+++ file_handling/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
 
 from core.base_serializer import BaseSerializer
 from file_handling.models import UploadedFile, FileContent, ZipFolder
@@ -9,19 +10,6 @@ class UploadedFileListSerializer(BaseSerializer):
         model = UploadedFile
         fields = ['guid', 'file', 'file_type', 'zip_folder']
         read_only_fields = ['guid']
-
-
-class UploadedFileDetailSerializer(BaseSerializer):
-    content = serializers.SerializerMethodField()
-
-    class Meta:
-        model = UploadedFile
-        fields = ['guid', 'file', 'file_type', 'zip_folder', 'content']
-        read_only_fields = ['guid']
-
-    def get_content(self, obj):
-        content_qs = FileContent.objects.filter(uploaded_file=obj)
-        return FileContentSerializer(content_qs, many=True).data
 
 
 class ZipFolderSerializer(BaseSerializer):
@@ -43,3 +31,17 @@ class FileContentSerializer(BaseSerializer):
         model = FileContent
         fields = ['guid', 'content']
         read_only_fields = ['guid']
+
+
+class UploadedFileDetailSerializer(BaseSerializer):
+    content = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UploadedFile
+        fields = ['guid', 'file', 'file_type', 'zip_folder', 'content']
+        read_only_fields = ['guid']
+
+    @extend_schema_field(FileContentSerializer(many=True))
+    def get_content(self, obj):
+        content_qs = FileContent.objects.filter(uploaded_file=obj)
+        return FileContentSerializer(content_qs, many=True).data
